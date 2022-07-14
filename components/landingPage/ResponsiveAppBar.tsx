4867ad084c6b044f67953799c176083fa768d6c0
stracciatella27 <schoko.bombe44@gmail.com>
2022-07-14 23:48:05 +0200

    Add landing Page

---
+++ components/landingPage/ResponsiveAppBar.tsx
@@ -0,0 +1,145 @@
+import { css } from "@emotion/react";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import Link from "next/link";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import Image from "next/image";
+
+const styles = {
+    root: css`
+        margin: 0 2em;
+        display: flex;
+        align-items: center;
+        justify-content: space-between; 
+    `,
+
+    heading: css`
+        display: flex;
+        gap: 0.5em;
+        font-weight: lighter;
+    `,
+
+    navigation: css`
+        display: flex;
+        gap: 4em;
+
+        a{
+        text-decoration: none;
+        font-weight: lighter;
+        color: black;
+        }
+
+        h3{
+        font-weight: lighter;
+        cursor: pointer;
+        }
+    `,
+
+    mobile: {
+
+        button: css`
+        background: none;
+            border: none;
+            padding: 0;
+            cursor: pointer;
+
+        .rotation{
+            animation: rotate 0.5s ease;
+        }
+
+        @keyframes rotate{
+            to{
+            transform: rotate(180deg);
+            }
+        }
+
+        .rotationBack{
+            animation: rotateBack 0.5s ease;
+        }
+
+        @keyframes rotateBack{
+            from{
+            transform: rotate(180deg);
+            }
+            to{
+            transform: rotate(0);
+            }
+        }
+        `,
+
+        list: css`
+            position: absolute;
+            right: 0;
+            background-color: white;
+
+            animation: scaleIn 0.5s ease;
+
+            @keyframes scaleIn{
+                from{
+                transform: scale(0);
+                }
+                to{
+                transform: scale(1);
+                }
+            }
+        `
+    }
+}
+
+type MenuItem = {
+    title: string;
+    link: string;
+}
+
+const checkWindowSize = (setMobile: Dispatch<SetStateAction<boolean>>) => {
+    setMobile(window.innerWidth - window.innerHeight > 0 ? true : false);
+}
+
+const ResponsiveAppBar = ({menuItems}: {menuItems: Array<MenuItem>}) => {
+    const MobileList = () => {
+        return <List css={styles.mobile.list}>
+          {menuItems.map((menuItem, index)=>{
+            return <ListItem key={`mobile_menu_item_${index}`} disablePadding>
+              <Link href={menuItem.link}>
+                <ListItemButton>
+                  <ListItemText primary={menuItem.title}/>
+                </ListItemButton>
+              </Link>
+            </ListItem>;
+          })}
+        </List>;
+    }
+
+    const [mobile, setMobile] = useState(false);
+    const [menuIconRotation, setMenuIconRotation] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("resize", ()=>{checkWindowSize(setMobile)});
+        checkWindowSize(setMobile);
+    }, []);
+
+
+    return <div css={styles.root}>
+        <h3 css={styles.heading}>
+            <Image src="/images/logo.svg" alt="logo" width="20px" height="20px"/>
+            Markdown Messenger
+        </h3>
+        {mobile ? <nav css={styles.navigation}>
+        {menuItems.map((menuItem, index)=>{
+            return <Link href={menuItem.link} key={`menu_item_${index}`}>
+            <h3>{menuItem.title}</h3>
+            </Link>
+        })}
+        </nav> : <div>
+            <button aria-label='open menu' css={styles.mobile.button} onClick={()=>{
+            setMenuIconRotation(!menuIconRotation);
+            }}>
+            <MenuIcon className={menuIconRotation ? 'rotation' : 'rotationBack'}/>
+            </button>
+            {menuIconRotation ? <MobileList /> : null}  
+        </div>
+        }
+    </div>;
+}
+
+export default ResponsiveAppBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,116 @@
-import { AppProps } from 'next/app';
-import Layout from '../components/landingPage/Layout';
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import ResponsiveAppBar from '../components/landingPage/ResponsiveAppBar';
 
-export default function LandingPage() {
-  return <div>landing page</div>;
+const menuItems = [
+  {
+      title: 'Messenger App',
+      link: '/messenger'
+  },
+  {
+      title: 'Login',
+      link: '/login'
+  },
+  {
+      title: 'Sign Up',
+      link: '/register'
+  },
+  {
+      title: 'GitHub',
+      link: 'https://github.com/stracciatella27/markdown-messenger'
+  }
+]
+
+const styles = {
+  root: css`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    section{
+      width: 100%;
+    }
+
+    h1{
+      font-weight: lighter;
+      font-size: 2.5em;
+      text-align: center;
+    }
+  `,
+
+  header: css`width: 100vw;`,
+
+  titleSection: css`
+    display: flex;
+    gap: 2em;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    h1{
+      width: 50vw;
+    }
+
+    div{
+      margin: 2em;
+    }
+  `,
+
+  poweredBySection: css`
+    margin: 4em 0;
+    display: flex;
+    flex-wrap: wrap;
+    
+    h1{
+      width: 100%;
+    }
+
+    div{
+      margin-top: 2em;
+      display: inherit;
+      flex-wrap: wrap;
+      gap: 4em;
+      justify-content: center;
+    }
+  `
+
 }
+
+const logoHeight = 100;
+
+const LandingPage = () => {
+  const [titleSectionHeight, setTitleSectionHeight] = useState(1000);
+
+  useEffect(()=>{
+    setTitleSectionHeight(window.innerHeight - (document.getElementById('app_bar')?.clientHeight as number));
+  }, []);
+
+  return <div css={styles.root}>
+    <header css={styles.header} id='app_bar'>
+      <ResponsiveAppBar menuItems={menuItems}/>
+    </header>
+    
+    <main>
+      <section css={css`${styles.titleSection} height: ${titleSectionHeight}px;`}>
+        <h1>The Worlds Most Highly Customizable Online Messenger.</h1>
+        <div>
+          <Image src="/images/landingPageLaptop.webp" width="500px" height={`${1440/2560*500}px`} alt="Minimalist art of laptop with Visual Studio Code"/>
+        </div>
+      </section>
+      <section css={styles.poweredBySection}>
+        <h1>Powered By</h1>
+        <div>
+          <Image src='/images/TechnologyIcons/nextjs-logo.png' alt='Next.js' width={`${1626/1347*logoHeight}px`} height={`${logoHeight}px`}/>
+          <Image src='/images/TechnologyIcons/react-logo.jpg' alt='React' width={`${960/540*logoHeight}px`} height={`${logoHeight}px`}/>
+          <Image src='/images/TechnologyIcons/cassandra-logo.png' alt='Cassandra' width={`${602/401*logoHeight}px`} height={`${logoHeight}px`}/>
+          <Image src='/images/TechnologyIcons/typescript-logo.png' alt='TypeScript' width={`${740/405*logoHeight}px`} height={`${logoHeight}px`}/>
+          <Image src='/images/TechnologyIcons/docker-logo.png' alt='Docker' width={`${400/331*logoHeight}px`} height={`${logoHeight}px`}/>
+          <Image src='/images/TechnologyIcons/mui-logo.png' alt='Material UI' width={`${1400/787*logoHeight}px`} height={`${logoHeight}px`}/>
+        </div>
+      </section>
+    </main>
+  </div>;
+}
+
+export default LandingPage;
