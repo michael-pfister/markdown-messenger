c1e30ee063e644e33d7219832b31e1816e356029
stracciatella27 <schoko.bombe44@gmail.com>
2022-07-18 17:22:50 +0200

    Fix 'Invalid Token Specified' Error

--- components/messenger/main/Chat.tsx
+++ components/messenger/main/Chat.tsx
@@ -44,7 +44,7 @@ export default function Chat(props: {selectedContact: ContactInformation}) {
   const [messagingInterfaceHeight, setMessagingInterfaceHeight] = useState(60);
 
   useEffect(() => {
-    fetch(`/api/settings?user=${(jwt_decode(Cookies.get('JSON_WEB_TOKEN') as string) as JwtPayload).user}`, {
+    Cookies.get('JSON_WEB_TOKEN') && fetch(`/api/settings?user=${(jwt_decode(Cookies.get('JSON_WEB_TOKEN') as string) as JwtPayload).user}`, {
       method: 'GET'
     }).then((response)=>{
        response.json().then(({avatar_url})=>{
